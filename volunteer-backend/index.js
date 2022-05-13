6913a87cdfd78845d3bb8a649ce86ad6281ee17a
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-05-13 23:59:16 +0800

    created a sample database with Prisma and connected to Heroku database

---
+++ volunteer-backend/index.js
@@ -0,0 +1,34 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "White Rabbit",
+      email: "whiterabbit@prisma.io",
+      posts: {
+        create: { title: "Red Queen Slave" },
+      },
+      profile: {
+        create: { bio: "I'm late! Very very late!" },
+      },
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
